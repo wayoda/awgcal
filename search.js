ff8121cf495e26186e5aab559550b1cb3e83cb06
civ0 <janis@marrek.de>
2016-03-06 17:12:00 +0100

    Added support for navigating the search results with the arrow keys

--- search.js
+++ search.js
@@ -1,17 +1,50 @@
 $(document).ready(function() {
 
-    // Standard Formsubmit verhindern
-    $("input#search").keydown(function(event) {
-        if (event.keyCode == 13) {
-            event.preventDefault();
-            return false;
-        }
-    });
-
     var jsonData;
     var selectedElement = -1;
 
-    // Livesuche
+    // Standard Formsubmit verhindern
+    $("input#search").keydown(function(e) {
+        if (e.keyCode == 13 || e.keyCode == 38 || e.keyCode == 40) {
+            e.preventDefault();
+        }
+
+        if (e.keyCode == 13) { // Enter
+            if (selectedElement == -1 && jsonData.length < 1) {
+                location = jsonData[0].url;
+            } else {
+                location = jsonData[selectedElement].url;
+            }
+            return false;
+        }
+
+        if (selectedElement != -2) { // Nur wenn Suchergebnisse vorhanden sind
+            if (e.keyCode == 38 && selectedElement > 0) { // up
+                var element = document.getElementById("el" + selectedElement);
+                element.classList.remove("active");
+                selectedElement--;
+                element = document.getElementById("el" + selectedElement);
+                element.classList.add("active");
+                return false;
+            }
+
+            if (e.keyCode == 40 && selectedElement < jsonData.length - 1) { // down
+                if (selectedElement == -1) {
+                    var element = document.getElementById("el0");
+                    element.classList.add("active");
+                    selectedElement = 0;
+                } else {
+                    var element = document.getElementById("el" + selectedElement);
+                    element.classList.remove("active");
+                    selectedElement++;
+                    element = document.getElementById("el" + selectedElement);
+                    element.classList.add("active");
+                }
+                return false;
+            }
+        }
+    });
+
     function search() {
         var query_value = $('input#search').val();
         $('b#search-string').text(query_value);
@@ -24,16 +57,19 @@ $(document).ready(function() {
                 },
                 cache: false,
                 success: function(response) {
-                    jsonData = JSON.parse(response);
-                    var htmlResult = "";
-
-                    var i;
-                    for (i = 0; i < jsonData.length; i++) {
-                        htmlResult += "<li class=\"dropdown-item\"><a href=\"" + jsonData[i].url + "\"><p>" + jsonData[i].name + "</p></li>";
-                    };
-
-                    selectedElement = 0;
-                    $("ul#results").html(htmlResult);
+                    if (response != "") {
+                        jsonData = JSON.parse(response);
+                        var htmlResult = "";
+                        var i;
+                        for (i = 0; i < jsonData.length; i++) {
+                            htmlResult += "<li class=\"dropdown-item\" id=\"el" + i + "\"><a href=\"" + jsonData[i].url + "\"><p>" + jsonData[i].name + "</p></li>";
+                        };
+                        $("ul#results").html(htmlResult);
+                        selectedElement = -1;
+                    } else {
+                        $("ul#results").html("<li class=\"dropdown-item\"><a><p>Keine Suchergebnisse</p></li>");
+                        selectedElement = -2;
+                    }
                 }
             });
         }
@@ -41,31 +77,19 @@ $(document).ready(function() {
     }
 
     $("input#search").on("keyup", function(e) {
-
-
-
-        // Zu ausgewaehltem Element gehen
-        if ((e.keyCode == 13)) {
-            if (selectedElement != -1) {
-                location = jsonData[selectedElement].url;
+        // Pfeiltasten abfangen
+        if (e.keyCode != 13 && e.keyCode != 37 && e.keyCode != 38 && e.keyCode != 39 && e.keyCode != 40) {
+            clearTimeout($.data(this, 'timer'));
+            var search_string = $(this).val();
+            if (search_string == '') {
+                $("ul#results").fadeOut();
+                $('h4#results-text').fadeOut();
+            } else {
+                $("ul#results").fadeIn();
+                $('h4#results-text').fadeIn();
+                $(this).data('timer', setTimeout(search, 100));
             }
         }
-
-        // Timeout setzen
-        clearTimeout($.data(this, 'timer'));
-
-        // Suchstring setzen
-        var search_string = $(this).val();
-
-        // Suchen
-        if (search_string == '') {
-            $("ul#results").fadeOut();
-            $('h4#results-text').fadeOut();
-        } else {
-            $("ul#results").fadeIn();
-            $('h4#results-text').fadeIn();
-            $(this).data('timer', setTimeout(search, 100));
-        };
     });
 
 });
