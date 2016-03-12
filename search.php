3be0ba56d2f540c8dd5fe7b12a9b5b3936ad6f27
civ0 <janis@marrek.de>
2016-03-12 11:32:36 +0100

    Use mysqli->prepare instead of plain mysqli->query. Added proper variables for creation of associative array.

--- search.php
+++ search.php
@@ -9,31 +9,25 @@ $key_name = "name";
 $key_url = "url";
 
 if (strlen($search_string) >= 1 && $search_string !== ' ') {
-	$query = 'SELECT * FROM strassen WHERE strasse LIKE "%' . $search_string . '%" AND hausnr is NULL LIMIT 10';
+	// $query = 'SELECT * FROM strassen WHERE strasse LIKE "%' . $search_string . '%" AND hausnr is NULL LIMIT 10';
 
-	$result = $mysqli->query($query);
-	while ($results = $result->fetch_assoc()) {
-		$result_array[] = $results;
+	// $result = $mysqli->query($query);
+
+	$stmt = $mysqli->prepare("SELECT strasse FROM strassen WHERE strasse LIKE CONCAT('%', ?, '%') AND hausnr is NULL LIMIT 10");
+	$stmt->bind_param("s", $search_string);
+	$stmt->execute();
+	$stmt->bind_result($res_strasse);
+
+	while ($stmt->fetch()) {
+		$data[] = [
+			$key_name => preg_replace("/" . $search_string . "/i", "<b class='highlight'>\$0</b>", $res_strasse),
+			$key_url => '/' . urlencode($res_strasse),
+		];
 	}
 
-	if (isset($result_array)) {
-
-		foreach ($result_array as $result) {
-			$temp = array(name => preg_replace("/" . $search_string . "/i", "<b class='highlight'>\$0</b>", $result['strasse']),
-				url => '/' . urlencode($result['strasse']));
-			$data[] = $temp;
-		}
-
+	if (isset($data)) {
 		$jsonData = json_encode($data);
 		echo ($jsonData);
-
-	} else {
-
-		$output = str_replace('nameString', '<b>Keine Ergebnisse</b>', $search_html);
-		$output = str_replace('<a href="urlString">', '', $output);
-
-		echo ($output);
-
 	}
 }
 
