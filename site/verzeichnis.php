7b0f1a767d013765f45c3c014a92cafd48975ad8
Dustin Schmidtberg <dustin@schmidtberg.de>
2016-08-04 18:06:58 +0200

    PHP Notice verzeichnis.php
    PHP Notice:  Undefined variable: i in awgcal/site/verzeichnis.php on
    line 9

--- site/verzeichnis.php
+++ site/verzeichnis.php
@@ -5,6 +5,7 @@ $stmt = $mysqli->query("SELECT DISTINCT strasse FROM strassen WHERE SUBSTRING(st
 $rows = ceil($stmt->num_rows/3);
 echo '  <div class="row">'."\n";
 echo '   <div class="col-sm-4">'."\n";
+$i = 0;
 while($row=$stmt->fetch_assoc()){
  if($i==$rows OR $i==$rows*2){
   echo '    </div>'."\n".'  <div class="col-sm-4">'."\n";
